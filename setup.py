16062818a351e46a224a506d37b37adf4665d897
Rick Wierenga <rick_wierenga@icloud.com>
2023-02-06 11:08:32 +0100

    Add dependencies

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
 from distutils.core import setup
 
+
 with open("README.md", "r") as f:
   long_description = f.read()
 
-setup(name='Pynopticon',
-      version='0.0.6',
-      description='Pynopticon',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Rick Wierenga',
-      author_email='rick_wierenga@icloud.com',
-      url='https://www.github.com/rickwierenga/pynopticon/',
-      packages=['pynopticon'],
-      entry_points={
-            'console_scripts': [
-                  'pynopticon-server = pynopticon.server:main'
-            ]   
-      } 
-   )
 
+setup(name="Pynopticon",
+  version="0.0.6",
+  description="Pynopticon",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author="Rick Wierenga",
+  author_email="rick_wierenga@icloud.com",
+  url="https://www.github.com/rickwierenga/pynopticon/",
+  packages=["pynopticon"],
+  install_requires=[
+    "google-api-python-client",
+    "apiclient",
+    "httplib2",
+    "opencv-python",
+    "oauth2client",
+    "flask"
+  ],
+  entry_points={
+    "console_scripts": [
+        "pynopticon-server = pynopticon.server:main"
+    ]
+  }
+)
