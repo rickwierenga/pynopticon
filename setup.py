fdebc72a0d4ddf80d7f6f947c62670a80e2f9793
Rick Wierenga <rick_wierenga@icloud.com>
2024-02-09 14:55:12 -0500

    version 0.0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 
 setup(name="Pynopticon",
-  version="0.0.7",
+  version="0.0.8",
   description="Pynopticon",
   long_description=long_description,
   long_description_content_type="text/markdown",
